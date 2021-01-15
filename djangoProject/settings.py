92729ed641a22e85b0a55b4e0be2bbf77240557a
matheusosp <matheusosp@users.noreply.github.com>
2021-01-15 20:00:53 -0300

    deploy

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,11 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 """
 EMAIL_HOST = 'localhost'
