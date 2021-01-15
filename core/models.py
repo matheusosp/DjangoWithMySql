a7641107290ca1cac2e4930afe26aaec99a6f2f6
matheusosp <matheusosp@users.noreply.github.com>
2021-01-15 15:33:25 -0300

    create model product

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from stdimage.models import StdImageField
+from django.db.models import signals
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Base(models.Model):
+    create = models.DateField("Date_created", auto_now_add=True)
+    modified = models.DateField("Data update", auto_now_add=True)
+    active = models.BooleanField("active", default=True)
+
+    class Meta:
+        abstract= True
+
+
+class Product(Base):
+    name = models.CharField('Name', max_length=100)
+    price = models.DecimalField('price', max_digits=8, decimal_places=2)
+    stock = models.IntegerField('stock')
+    img = StdImageField("Img", upload_to='products', variations={'thumb': (124,124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+def product_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.name)
+
+
+signals.pre_save.connect(product_pre_save, sender=Product)
